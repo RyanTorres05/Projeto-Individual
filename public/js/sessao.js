2868e9ee03311f15de9bda645ca9b30f2209563a
RyanTorres05 <ryan.costa@sptech.school>
2024-05-23 11:52:47 -0300

    fazendo alteracoes no login

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -3,10 +3,10 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    // var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+        // b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
     }
@@ -18,10 +18,10 @@ function limparSessao() {
 }
 
 // carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+// function aguardar() {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     // divAguardar.style.display = "flex";
+// }
 
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
