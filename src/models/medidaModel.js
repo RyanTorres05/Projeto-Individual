c218fb7b0b001ef816c862bb7b33d010e67ac0fc
RyanTorres05 <ryan.costa@sptech.school>
2024-05-31 20:04:19 -0300

    iniciando a dashboard

---
+++ src/models/medidaModel.js
@@ -0,0 +1,28 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas() {
+
+    var instrucaoSql = `SELECT * from pontuacao;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// function buscarMedidasEmTempoReal(idAquario) {
+
+//     var instrucaoSql = `SELECT 
+//         dht11_temperatura as temperatura, 
+//         dht11_umidade as umidade,
+//                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+//                         fk_aquario 
+//                         FROM medida WHERE fk_aquario = ${idAquario} 
+//                     ORDER BY id DESC LIMIT 1`;
+
+//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//     return database.executar(instrucaoSql);
+// }
+
+module.exports = {
+    buscarUltimasMedidas,
+    // buscarMedidasEmTempoReal
+}
